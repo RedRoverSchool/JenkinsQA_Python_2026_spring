c8b3912f40d83b27333fbd85b43f60e01344e09b
Alex-Aladdin <blackbeard007@bk.ru>
2026-04-11 18:04:42 +0300

    Добавлен тест отправки формы с длинным паролем

--- tests/test_password_field_max_lenght.py
+++ tests/test_password_field_max_lenght.py
@@ -1,7 +1,9 @@
+from telnetlib import EC
 import pytest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 URL = "http://www.selenium.dev/selenium/web/web-form.html"
 
@@ -25,3 +27,15 @@ def test_password_field_max_lenght(browser):
     entered_value = password_field.get_attribute("value")
     time.sleep(3)
     assert len(entered_value) == 100
+
+def test_submit_with_long_password(browser):
+    browser.get(URL)
+    password_field.send_keys(long_password)
+
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    submit_button.click()
+
+    message = WebDriverWait(browser, 5).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#message"))
+    )
+    assert message.text == "Recieved!"
