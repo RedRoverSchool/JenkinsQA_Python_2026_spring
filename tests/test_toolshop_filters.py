5335c891d5c9b231359937a8d0107bd98f7ffb3e
tanyaIlina <4199ti@gmail.com>
2026-04-11 22:31:28 +0200

    fix Power Tools checkbox

--- tests/test_toolshop_filters.py
+++ tests/test_toolshop_filters.py
@@ -1,12 +1,19 @@
 import time
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_toolshop_filters(browser):
     browser.get("https://practicesoftwaretesting.com/")
     time.sleep(1)
-    power_tools_checkbox = browser.find_element(By.CSS_SELECTOR, '[data-test="category-01KNY366MVN0YAXXH9MC8HC1RB"]')
+    power_tools_checkbox = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((
+            By.XPATH,
+            '//label[contains(normalize-space(.), "Power Tools")]//input[@type="checkbox"]'
+        ))
+    )
 
     power_tools_checkbox.click()
 
