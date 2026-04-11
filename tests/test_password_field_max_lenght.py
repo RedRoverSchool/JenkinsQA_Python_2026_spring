b8c6411722216b86b88128aafff3cee3d8be3d12
Alex-Aladdin <blackbeard007@bk.ru>
2026-04-11 18:26:30 +0300

    Исправлены ошибки в тестах (driver, опечатка Received)

--- tests/test_password_field_max_lenght.py
+++ tests/test_password_field_max_lenght.py
@@ -1,6 +1,5 @@
-from telnetlib import EC
+from selenium.webdriver.support import expected_conditions as EC
 import pytest
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -11,31 +10,26 @@ URL = "http://www.selenium.dev/selenium/web/web-form.html"
 def driver():
     driver = webdriver.Chrome()
     yield driver
-    driver.close()
+    driver.quit()
 
-def test_password_field_max_lenght(browser):
-    browser.get(URL)
-    password_field = browser.find_element(By.CSS_SELECTOR, "input[name='my-password']")
-    time.sleep(3)
-    maxlenght = password_field.get_attribute("maxlength")
-
-    assert maxlenght is None
-
-    long_password = 'a'*100
-    time.sleep(3)
+def test_password_field_max_length(driver):
+    driver.get(URL)
+    password_field = driver.find_element(By.CSS_SELECTOR, "input[name='my-password']")
+    maxlength = password_field.get_attribute("maxlength")
+    assert maxlength is None
+    long_password = 'a' * 100
     password_field.send_keys(long_password)
     entered_value = password_field.get_attribute("value")
-    time.sleep(3)
     assert len(entered_value) == 100
 
-def test_submit_with_long_password(browser):
-    browser.get(URL)
+def test_submit_with_long_password(driver):
+    driver.get(URL)
+    password_field = driver.find_element(By.CSS_SELECTOR, "input[name='my-password']")
+    long_password = 'a' * 100
     password_field.send_keys(long_password)
-
-    submit_button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    submit_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
     submit_button.click()
-
-    message = WebDriverWait(browser, 5).until(
+    message = WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, "#message"))
     )
-    assert message.text == "Recieved!"
+    assert message.text == "Received!"
