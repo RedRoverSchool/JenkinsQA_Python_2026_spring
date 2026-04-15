d7434b889766031a1a6691cdaf455482ef5caedf
SaliewRK <saliewrk@gmail.com>
2026-04-15 20:05:57 +0200

    add first test

--- tests/test_check_tool_card.py
+++ tests/test_check_tool_card.py
@@ -1,14 +1,12 @@
 import time
 from selenium.webdriver.common.by import By
 
-
-
 def test_get_url(browser):
 
     browser.get("https://practicesoftwaretesting.com")
 
-    product_name = browser.find_element(By.XPATH, '//*[@data-test="product-name"]')
+    product_name = browser.find_element(By.XPATH, '//*[@data-test="product-name"]') # клик на карточку товара
     product_name.click()
 
-    add_to_cart = browser.find_element(By.XPATH, '//*[@data-test="add-to-cart"]')
+    add_to_cart = browser.find_element(By.XPATH, '//*[@data-test="add-to-cart"]') # проверяем наличие кнопки Добавить в корзину
     assert add_to_cart.is_displayed(), "Button is missing"
