5f15c616d051f26df572625886e0d757cb9d776f
Adelya Sensation <omsksmp@gmail.com>
2026-04-19 15:18:03 +0300

    TC_11.006.01 | Welcome Dashboard > Set Up an Agent > Create new Agent2

--- tests/test_set_up_agent.py
+++ tests/test_set_up_agent.py
@@ -1,9 +1,11 @@
-
 from selenium.webdriver.common.by import By
 
+
 def test_create_agent(browser):
+    agent_name = 'agent name'
+
     browser.find_element(By.XPATH, '//a[@href="computer/new"]').click()
-    browser.find_element(By.NAME, 'name').send_keys('agent name')
+    browser.find_element(By.NAME, 'name').send_keys(agent_name)
     browser.find_element(By.XPATH, "//input[@id='hudson.slaves.DumbSlave']/following-sibling::label").click()
     browser.find_element(By.NAME, 'Submit').click()
 
@@ -11,3 +13,4 @@ def test_create_agent(browser):
     browser.find_element(By.NAME, 'Submit').click()
 
     assert "/computer/" in browser.current_url
+    assert browser.find_element(By.XPATH, f"//a[contains(text(), '{agent_name}')]").is_displayed()
