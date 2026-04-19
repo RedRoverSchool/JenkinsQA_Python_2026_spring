1d20d148523fdca652724a0faa84dc4df39ddeb8
Miguel19774 <pugbag1@gmail.com>
2026-04-19 17:23:17 +0200

    add negative test for empty item name

--- tests/test_empty_name_warning_message_miguel.py
+++ tests/test_empty_name_warning_message_miguel.py
@@ -1,31 +1,10 @@
-# ID: TC_01.001.02 | New Item > Creatе a new item > Validate empty item name
-#
-# Preconditions:
-# User is on the New Item page
-#
-# Description:
-#
-# Verify that empty item name is not allowed
-#
-# Steps:
-# Open New Item page.
-# Leave Item Name field empty.
-# Select any item type.
-# Observe the validation result.
-# Expected Result:
-# Warning message that field cannot be empty
-# OK button is disabled
-# Comments / Notes:
-# Observe the validation result.
-#
-# Acceptance Criteria:
-# If the item name field is blank, a warning message appears indicating the name is invalid, and the "OK" button must remain disabled.
-
 
 from selenium.webdriver.common.by import By
 
 def test_empty_item_mame(browser):
-    browser.get("http://localhost:8080/view/all/newJob")
+    element_button = browser.find_element(By.XPATH, '//a[contains(., "New Item")]')
+    element_button.click()
+    assert "/view/all/newJob" in browser.current_url
     element = browser.find_element(By.ID,"name")
     assert element.get_attribute("value") == ""
     folder = browser.find_element(By.XPATH,"//li[contains(@class, 'folder_Folder')]")
