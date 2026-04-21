8d511f9cae6dd175260c577727acf5818e6eb892
Irina Bibikova <irinabibikova1985@gmail.com>
2026-04-21 17:40:16 +0100

    Add @pytest.mark.skip()

--- tests/test_new_freestyle_project.py
+++ tests/test_new_freestyle_project.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.mark.skip()
 def test_new_freestyle_project(browser):
     driver = browser
     wait = WebDriverWait(driver, 10)
