171797534d4c7cfe7c8277c1bb99459964cc48d0
anika <anika.mugu@gmail.com>
2026-04-22 20:46:06 +0300

    add auto test

--- tests/test_create_new_item.py
+++ tests/test_create_new_item.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 @pytest.mark.skip
 @pytest.mark.parametrize("invalid_char", ["?", "*", "/", "|", "!", "%", "&", ";", ":"])
@@ -23,3 +24,33 @@ def test_create_new_item_validate_unsupported_special_characters(browser, invali
     assert warning.is_displayed()
     assert "unsafe character" in warning.text
     assert ok_button.get_attribute("disabled") is not None
+
+
+def test_create_new_item(browser):
+    element_button = browser.find_element(By.CLASS_NAME, "task-icon-link")
+    element_button.click()
+
+    input_item_name = browser.find_element(By.ID, 'name')
+    input_item_name.send_keys('New_Project')
+
+    select_item_type = browser.find_element(By.CLASS_NAME, 'hudson_model_FreeStyleProject')
+    select_item_type.click()
+
+    ok_button = browser.find_element(By.ID, 'ok-button')
+    ok_button.click()
+
+    save_button = browser.find_element(By.XPATH, "//*[@value='Save']")
+    save_button.click()
+
+    WebDriverWait(browser, 10).until(
+        EC.url_contains("/job/")
+    )
+
+    time.sleep(2)
+
+    project_title = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, "h1"))
+    )
+
+    print(f"Найден заголовок: '{project_title.text}'")
+    assert "New_Project" in project_title.text
