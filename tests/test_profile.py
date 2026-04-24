7eef286f1f5db68cd70254764cc4635d18b32d9d
TatyanaBron <tatyana.bronnikova@gmail.com>
2026-04-23 22:01:48 -0700

    updated first jenkins profile test

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -5,6 +5,7 @@ def test_profile(browser):
     profile = browser.find_element(By.CLASS_NAME, "jenkins-avatar")
     profile.click()
     option = browser.find_element(By.XPATH, '// *[ @ id = "tasks"] / div[1] / span / a / span[2]')
-    assert option.text == "Profile"
+    new_item = option.text
+    assert new_item == "Profile"
     # element = browser.find_element(By.XPATH, '//*[@id="breadcrumbs"]/li/span')
     # assert element.text == "Tatyana"
