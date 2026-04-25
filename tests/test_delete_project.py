e93f8b5622a7b29a1e043d98d25f613e19f93f0e
Sergei Krivonosov <ksva690@gmail.com>
2026-04-25 13:26:34 +0200

    @pytest.mark.skip()

--- tests/test_delete_project.py
+++ tests/test_delete_project.py
@@ -16,29 +16,29 @@ DELETE_JOB = (By.XPATH, "//a[contains(@data-title, 'Delete')]")
 NOT_FOUND_JOB_TITLE = (By.XPATH, "//h2[text()='Not Found']")
 CONFIRM_DELETE_BUTTON = (By.XPATH, "//button[@data-id='ok']")
 CANCEL_DELETE_BUTTON = (By.XPATH, "//button[@data-id='cancel']")
-
+@pytest.mark.skip()
 @pytest.fixture
 def click(browser):
     def clicking(element, duration):
         WebDriverWait(browser, duration).until(EC.element_to_be_clickable(element)).click()
     return clicking
-
+@pytest.mark.skip()
 @pytest.fixture
 def fill(browser):
     def filling(element, text, duration):
         WebDriverWait(browser, duration).until(EC.visibility_of_element_located(element)).send_keys(text)
     return filling
-
+@pytest.mark.skip()
 @pytest.fixture
 def check_visibility(browser):
     def checking(element, duration):
         WebDriverWait(browser, duration).until(EC.visibility_of_element_located(element))
     return checking
-
+@pytest.mark.skip()
 def generate_project_name():
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(10))
-
+@pytest.mark.skip()
 def create_job(click, fill, check_visibility, current_project_name):
 
     click(NEW_ITEM_BUTTON, 5)
@@ -52,7 +52,7 @@ def create_job(click, fill, check_visibility, current_project_name):
     check_visibility(JOB_TITLE, 5)
     click(JENKINS_LOGO, 5)
     check_visibility(JENKINS_LOGO, 5)
-
+@pytest.mark.skip()
 def test_delete_job(browser, click, fill, check_visibility):
 
     current_project_name = generate_project_name()
