fdab8f8dd24a1fdb82ca218abd395816ae7b0201
Sergei Krivonosov <ksva690@gmail.com>
2026-04-25 22:33:55 +0200

    after review_2

--- tests/test_new_item.py
+++ tests/test_new_item.py
@@ -1,9 +1,78 @@
+import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
-def test_new_item_page_opens_after_click(browser):
-    element = browser.find_element(By.XPATH, '//a[contains(., "New Item")]')
-    element.click()
-    header = browser.find_element(By.XPATH, '//h1[contains(., "New Item")]')
-    assert "http://localhost:8080/view/all/newJob" in browser.current_url
-    assert "New Item" in browser.title
-    assert header.text == "New Item"
+def locators_storage(browser):
+    browser.find_element(By.CLASS_NAME, "task-icon-link").click()
+
+    test_folder_name = "Folder"
+    browser.find_element(By.ID, "name").send_keys(test_folder_name)
+    browser.find_element(By.XPATH, "//*[@id='j-add-item-type-nested-projects']/ul/li[1]/div[2]").click()
+    browser.find_element(By.ID, "ok-button").click()
+
+    browser.find_element(By.XPATH, "//*[@id='main-panel']/form/div[1]/div[2]/div/div[2]/input").send_keys("Folder")
+    browser.find_element(By.XPATH, "//*[@id='main-panel']/form/div[1]/div[3]/div[2]/textarea").send_keys("Folder")
+    browser.find_element(By.XPATH, "//*[@id='bottom-sticker']/div/button[1]").click()
+
+    created_folder_name = browser.find_element(By.XPATH, "//*[@id='main-panel']/div[1]/div/h1").text
+
+    browser.find_element(By.XPATH, "//*[@id='page-header']/div[1]/div/a").click()
+    dashboard_item = browser.find_element(By.XPATH, "//*[@id='projectstatus']/tbody")
+
+    return test_folder_name, created_folder_name, dashboard_item
+
+
+def test_create_folder_with_valid_name(browser):
+    test_folder_name, created_folder_name, dashboard_item = locators_storage(browser)
+
+    assert test_folder_name == created_folder_name, "Folder name does not match"
+    assert dashboard_item.is_displayed(), "Dashboard is empty"
+
+def test_create_from_copy(browser):
+    wait = WebDriverWait(browser, 10)
+
+    wait.until(
+        EC.element_to_be_clickable((By.LINK_TEXT, "New Item"))
+    ).click()
+
+    wait.until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="name"]'))
+    ).send_keys('My first folder')
+
+    wait.until(
+        EC.element_to_be_clickable((By.CLASS_NAME, 'com_cloudbees_hudson_plugins_folder_Folder'))
+    ).click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'ok-button'))
+    ).click()
+
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//button[@value="Save"]'))
+    ).click()
+    time.sleep(2)
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'jenkins-head-icon'))
+    ).click()
+
+    browser.find_element(By.LINK_TEXT, "New Item").click()
+
+    browser.find_element(By.ID, 'name').send_keys('Folder from copy')
+
+    browser.find_element(By.ID, 'from').send_keys('My first folder')
+    browser.find_element(By.ID, 'from').send_keys(Keys.ENTER)
+
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, '//button[@value="Save"]'))
+    ).click()
+    time.sleep(2)
+
+    wait.until(
+        EC.element_to_be_clickable((By.ID, 'jenkins-head-icon'))
+    ).click()
+
+    folder = wait.until(EC.visibility_of_element_located((By.LINK_TEXT, 'Folder from copy')))
+    assert folder.is_displayed()
