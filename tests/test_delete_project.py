b6aa25e2003484cba30cc2ad075dac3cf8abf303
Sergei Krivonosov <ksva690@gmail.com>
2026-04-25 13:24:47 +0200

    @pytest.mark.skip()

--- tests/test_delete_project.py
+++ tests/test_delete_project.py
@@ -5,34 +5,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 JENKINS_LOGO = (By.XPATH, "//span[@class='jenkins-mobile-hide']")
 NEW_ITEM_BUTTON = (By.XPATH, "//a[@it]")
 INPUT_NEW_ITEM_FIELD = (By.XPATH, "//input[@id='name']")
 PIPELINE_ITEM_TYPE = (By.XPATH, "((//ul[@class='j-item-options'])[1]//li)[1]")
 JOB_TITLE = (By.XPATH, "//h1[@class='job-index-headline page-headline']")
-
+@pytest.mark.skip()
 @pytest.fixture
 def click(browser):
     def clicking(element, duration):
         WebDriverWait(browser, duration).until(EC.element_to_be_clickable(element)).click()
     return clicking
-
+@pytest.mark.skip()
 @pytest.fixture
 def fill(browser):
     def filling(element, text, duration):
         WebDriverWait(browser, duration).until(EC.visibility_of_element_located(element)).send_keys(text)
     return filling
-
+@pytest.mark.skip()
 @pytest.fixture
 def check_visibility(browser):
     def checking(element, duration):
         WebDriverWait(browser, duration).until(EC.visibility_of_element_located(element))
     return checking
-
+@pytest.mark.skip()
 def generate_project_name():
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(10))
-
+@pytest.mark.skip()
 def create_job(click, fill, check_visibility, current_project_name):
 
     click(NEW_ITEM_BUTTON, 5)
@@ -46,7 +47,7 @@ def create_job(click, fill, check_visibility, current_project_name):
     check_visibility(JOB_TITLE, 5)
     click(JENKINS_LOGO, 5)
     check_visibility(JENKINS_LOGO, 5)
-
+@pytest.mark.skip()
 def test_delete_job(browser, click, fill, check_visibility):
 
     current_project_name = generate_project_name()
