322c1d7dc1040eab21146bdae8999ecfd5dfcccd
Руслан <aryslan_85@mail.ru>
2026-04-26 17:45:33 +0300

    add simple test

--- tests/simple_test_khasanov.py
+++ tests/simple_test_khasanov.py
@@ -14,7 +14,7 @@ text_box = driver.find_element(by=By.NAME, value="my-text")
 submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
 text_box.send_keys("Selenium")
-time.sleep(7)
+time.sleep(6)
 submit_button.click()
 
 message = driver.find_element(by=By.ID, value="message")
