6b7e3e4b8e6f843be4e3cf99978f5d16bb30b0ee
NatTestCode <natashakoroleva2022@gmail.com>
2026-04-25 21:43:32 -0400

    added new test - Change theme through Appearance page

---
+++ tests/test_change_theme_through_appearance_page.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+def test_quick_change_theme(browser):
+
+    user_menu = browser.find_element(By.ID, "root-action-UserAction")
+    ActionChains(browser).move_to_element(user_menu).perform()
+
+    browser.find_element(By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']").click()
+    browser.find_element(By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]").click()
+    time.sleep(1)
+
+    theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
+
+    assert theme.lower() == "dark"
