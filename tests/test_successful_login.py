b09cea35f8ddd2b8dfad22e963cf67d76e80cf59
MrSaN4eZ <MrSaN4eZ@MrSan4eZ>
2026-04-27 13:00:07 +0300

    TC_08.001.01 | Sign in/out > Sign in > Successful login to Jenkins

--- tests/test_successful_login.py
+++ tests/test_successful_login.py
@@ -1,19 +1,13 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
 
+
 def test_successful_login(browser):
-    wait = WebDriverWait(browser, 5)
-    user_action = wait.until(
-        EC.visibility_of_element_located((By.ID, "root-action-UserAction"))
-    )
-    ActionChains(browser).move_to_element(user_action).perform()
-    logout = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href='/logout']"))
-    )
-    logout.click()
+    actions = ActionChains(browser)
+    menu_item = browser.find_element(By.ID, "root-action-UserAction")
+    actions.move_to_element(menu_item).perform()
+    browser.find_element(By.CSS_SELECTOR, "a[href='/logout']").click()
 
     username_input = browser.find_element(By.CSS_SELECTOR, "#j_username")
     username_input.send_keys("admin")
