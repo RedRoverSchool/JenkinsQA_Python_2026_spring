7b3e44ab39b57e6f415180649a827a1a4ed3df30
NatTestCode <natashakoroleva2022@gmail.com>
2026-04-27 13:11:13 -0400

    Change website theme through User menu > Appearance page option

--- tests/test_change_theme_through_appearance_page.py
+++ tests/test_change_theme_through_appearance_page.py
@@ -4,14 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-def test_change_theme_through_appearance_pag(browser):
+def test_change_theme_through_appearance_page(browser):
 
     user_menu = browser.find_element(By.ID, "root-action-UserAction")
     ActionChains(browser).move_to_element(user_menu).perform()
 
     browser.find_element(By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']").click()
     browser.find_element(By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]").click()
-    time.sleep(1)
+    browser.find_element(By.XPATH, "//*[@class='jenkins-button apply-button']").click()
 
     theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
 
