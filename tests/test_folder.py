d48ffa9fb22b573812edadfcfaa75983052827f3
Sergei Krivonosov <ksva690@gmail.com>
2026-04-27 09:08:33 +0200

    new test

--- tests/test_folder.py
+++ tests/test_folder.py
@@ -1,7 +1,9 @@
 import pytest
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 FOLDER_NAME = "TestFolder"
 
