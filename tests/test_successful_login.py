ff8e11be6852503fae1de99fb77f0d048968705c
MrSaN4eZ <MrSaN4eZ@MrSan4eZ>
2026-04-27 17:04:37 +0300

    TC_08.001.01 | Sign in/out > Sign in > Successful login to Jenkins

--- tests/test_successful_login.py
+++ tests/test_successful_login.py
@@ -9,13 +9,14 @@ def test_successful_login(browser):
     actions.move_to_element(menu_item).perform()
     browser.find_element(By.CSS_SELECTOR, "a[href='/logout']").click()
 
-    username_input = browser.find_element(By.CSS_SELECTOR, "#j_username")
-    username_input.send_keys("admin")
+    browser.find_element(By.CSS_SELECTOR, "#j_username").send_keys("admin")
 
-    password_input = browser.find_element(By.CSS_SELECTOR, "#j_password")
-    password_input.send_keys("admin")
+
+    browser.find_element(By.CSS_SELECTOR, "#j_password").send_keys("admin")
+    password_input.
 
     browser.find_element(By.XPATH, "//button[@type='submit']").click()
+
 
     label = browser.find_element(By.CSS_SELECTOR, "div.empty-state-block > h1")
 
