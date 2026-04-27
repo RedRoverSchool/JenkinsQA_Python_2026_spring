0c49a84cc48b843fab3a97b3b0ab3871f8739d0c
NatTestCode <natashakoroleva2022@gmail.com>
2026-04-27 13:27:43 -0400

    Change website theme in the header through User menu > Change Theme

--- tests/test_header_quick_change_theme.py
+++ tests/test_header_quick_change_theme.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-def test_quick_change_theme(browser):
+def test_header_quick_change_theme(browser):
 
     user_menu = browser.find_element(By.ID, "root-action-UserAction")
     ActionChains(browser).move_to_element(user_menu).perform()
@@ -12,5 +12,3 @@ def test_quick_change_theme(browser):
     theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
 
     assert theme.lower() == "dark"
-
-    # browser.find_element(By.XPATH, "//*[contains(text(), 'Light') or contains (text(), 'light')]").click()
