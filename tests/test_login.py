40081178313f86bce0d4b63f8978bc0eb7d263ab
MrSaN4eZ <MrSaN4eZ@MrSan4eZ>
2026-04-27 19:22:39 +0300

    TC_08.001.01 | Sign in/out > Sign in > Successful login to Jenkins3

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+@pytest.mark.skip()
 def test_successful_login(browser):
     actions = ActionChains(browser)
     menu_item = browser.find_element(By.ID, "root-action-UserAction")
