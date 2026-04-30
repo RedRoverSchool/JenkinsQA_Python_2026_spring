aefbd63c94a985192e7339bf94d3517615bf47fa
NatTestCode <natashakoroleva2022@gmail.com>
2026-04-30 17:24:07 -0400

    Added new test: Check updated theme after logging out and logging in

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from common.jenkins_utils import login
-
-
+from common.project_utils import get_url
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_header_quick_change_theme(browser):
 
@@ -35,21 +36,18 @@ def test_check_updated_theme_after_logout(browser):
     user_menu = browser.find_element(By.ID, "root-action-UserAction")
     ActionChains(browser).move_to_element(user_menu).perform()
 
-    browser.find_element(By.XPATH, "//*[contains(text(), 'Dark') or contains (text(), 'dark')]").click()
-    theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
-    assert theme.lower() == "dark"
+    browser.find_element(By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']").click()
+    browser.find_element(By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]").click()
+    browser.find_element(By.XPATH, "//*[@class='jenkins-button apply-button']").click()
+    browser.find_element(By.XPATH, "//*[@id='bottom-sticker']/div/button[1]").click()
 
-    browser.get("http://localhost:8080/logout")
+    browser.get(get_url() + "/logout")
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.NAME, "j_username")))
 
-    username_fields = browser.find_element(By.NAME, "j_username")
-    password_fields = browser.find_element(By.NAME,"j_password")
+    login(browser)
 
-    if username_fields and password_fields:
-        username_fields.send_keys("Admin")
-        password_fields.send_keys("Victoria33#")
-
-        browser.find_element(By.NAME,"Submit").click()
-
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "root-action-UserAction")))
 
     theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
     assert theme.lower() == "dark"
