5ba9cb67f4c4a155a89a5964ea6ad3f873f4dc97
NatTestCode <natashakoroleva2022@gmail.com>
2026-05-02 16:09:51 -0400

    Added new test: Check updated theme after logging out and logging in

--- tests/test_header.py
+++ tests/test_header.py
@@ -8,39 +8,39 @@ from common.project_utils import get_url
 
 def test_header_quick_change_theme(browser):
 
-    user_menu = browser.find_element(By.ID, "root-action-UserAction")
+    user_menu = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "root-action-UserAction")))
     ActionChains(browser).move_to_element(user_menu).perform()
 
-    browser.find_element(By.XPATH, "//*[contains(text(), 'Dark') or contains (text(), 'dark')]").click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Dark') or contains (text(), 'dark')]"))).click()
 
-    theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
+    theme = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "html"))).get_attribute("data-theme")
 
     assert theme.lower() == "dark"
 
 
 def test_header_change_theme_through_appearance_page(browser):
 
-    user_menu = browser.find_element(By.ID, "root-action-UserAction")
+    user_menu = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "root-action-UserAction")))
     ActionChains(browser).move_to_element(user_menu).perform()
 
-    browser.find_element(By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']").click()
-    browser.find_element(By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]").click()
-    browser.find_element(By.XPATH, "//*[@class='jenkins-button apply-button']").click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']"))).click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]"))).click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@class='jenkins-button apply-button']"))).click()
 
-    theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
+    theme = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "html"))).get_attribute("data-theme")
 
     assert theme.lower() == "dark"
 
 
 def test_check_updated_theme_after_logout(browser):
 
-    user_menu = browser.find_element(By.ID, "root-action-UserAction")
+    user_menu = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "root-action-UserAction")))
     ActionChains(browser).move_to_element(user_menu).perform()
 
-    browser.find_element(By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']").click()
-    browser.find_element(By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]").click()
-    browser.find_element(By.XPATH, "//*[@class='jenkins-button apply-button']").click()
-    browser.find_element(By.XPATH, "//*[@id='bottom-sticker']/div/button[1]").click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'jenkins-dropdown')]//*[normalize-space()='Appearance']"))).click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='app-theme-picker__item']//*[contains(text(),'Dark') or contains(text(), 'dark')]"))).click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@class='jenkins-button apply-button']"))).click()
+    WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='bottom-sticker']/div/button[1]"))).click()
 
     browser.get(get_url().rstrip("/") + "/logout")
     browser.get(get_url().rstrip("/") + "/login")
@@ -51,7 +51,7 @@ def test_check_updated_theme_after_logout(browser):
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.ID, "root-action-UserAction")))
 
-    theme = browser.find_element(By.TAG_NAME, "html").get_attribute("data-theme")
+    theme = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.TAG_NAME, "html").get_attribute("data-theme")))
     assert theme.lower() == "dark"
 
 
