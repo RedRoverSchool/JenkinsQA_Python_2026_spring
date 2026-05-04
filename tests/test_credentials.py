e202c22c877ebb67a59d26152878cf7b933cc698
Akiko <iakikoaokii@gmail.com>
2026-05-04 21:49:50 +0400

    fix: change wait to visibility_of_element_located

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -108,7 +108,7 @@ def test_edit_enable_checkbox(browser):
     username_before = tags_element.text
 
     update_button = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//*[@title = 'Update credential']"))
+        EC.visibility_of_element_located((By.XPATH, "//*[@title = 'Update credential']"))
     )
     update_button.click()
 
