6df7d7053b94c6f9b5c7b3367c08401440db2c53
Sergey <space-researcher@i.ua>
2026-05-05 15:41:52 +0300

    Revert "Fixing my test and skipping failed tests."
    This reverts commit 0a51d0de62f11d9371bd229b89383f6ae533d933.

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -84,8 +84,6 @@ def test_create_duplicate_id_error_validation(browser):
 @pytest.mark.parametrize("special_characters ",[
     "!", "%", "&", "#", "@", "*"
 ])
-
-@pytest.mark.skip
 def test_create_id_with_special_characters(browser, special_characters):
     expected_error = "Unacceptable characters"
 
