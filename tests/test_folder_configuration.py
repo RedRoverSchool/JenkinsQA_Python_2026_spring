a8fd7994bb20710a1277b63e28852eb0173291c3
kiramitt <kiramitt@gmail.com>
2026-05-10 23:59:08 +0300

    AT_05.001.04 | Folder Configuration > Display Name and Description > Delete Display Name From Folder

--- tests/test_folder_configuration.py
+++ tests/test_folder_configuration.py
@@ -20,3 +20,13 @@ def test_add_display_name_to_folder(browser):
         [line for line in browser.find_element(By.ID, "main-panel").text.split('\n') if
          line.startswith("Folder name: ")][0]
     assert folder_name_line == f"Folder name: {FOLDER_NAME}"
+
+@pytest.mark.dependency(depends=["test_add_display_name_to_folder"])
+def test_delete_display_name_from_folder(browser):
+    browser.find_element(By.XPATH, f"//a[contains(@href, '/{FOLDER_NAME}')]").click()
+    browser.find_element(By.XPATH, "//a[contains(@href, '/configure')]").click()
+
+    browser.find_element(By.NAME, "_.displayNameOrNull").clear()
+    browser.find_element(By.NAME, "Submit").click()
+
+    assert browser.find_element(By.CLASS_NAME, "job-index-headline").text == FOLDER_NAME
