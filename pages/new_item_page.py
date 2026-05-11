ef95b5ba978686e79d0876c8c2fa394cfb466390
GH-Vasilii <vvs-87@list.ru>
2026-05-11 19:52:29 +0300

    add new test

--- pages/new_item_page.py
+++ pages/new_item_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 from pages.freestyle_config_page import FreestyleConfigPage
+from pages.pipeline_config_page import PipelineConfigPage
 
 
 class NewItemPage(BasePage):
@@ -16,3 +17,9 @@ class NewItemPage(BasePage):
         self.driver.find_element(By.ID, "ok-button").click()
 
         return FreestyleConfigPage(self.driver)
+
+    def select_pipeline_and_ok_click(self):
+        self.driver.find_element(By.CLASS_NAME, "org_jenkinsci_plugins_workflow_job_WorkflowJob").click()
+        self.driver.find_element(By.ID, "ok-button").click()
+
+        return PipelineConfigPage(self.driver)
--- tests/test_create_new_pipeline.py
+++ tests/test_create_new_pipeline.py
@@ -1,90 +1,22 @@
 import pytest
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-ITEM_NAME = "Test Pipeline"
+from pages.home_page import HomePage
 
-
-@pytest.fixture
-def wait(browser):
-    return WebDriverWait(browser, 5)
+PIPELINE_NAME = "Test Pipeline"
+DESCRIPTION = "Description Pipeline"
 
 
 @pytest.mark.dependency()
-def test_create_new_pipeline(browser, wait):
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[.//span[normalize-space()='New Item']]"))
-    ).click()
-    browser.find_element(By.CSS_SELECTOR, "button#ok-button[disabled]")
-    browser.find_element(By.CSS_SELECTOR, "input#name").send_keys(ITEM_NAME)
-    browser.find_element(By.XPATH, "//li[.//span[normalize-space()='Pipeline']]").click()
-    browser.find_element(By.CSS_SELECTOR, "button#ok-button").click()
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@name='Apply']"))
-    ).click()
-    browser.find_element(By.CSS_SELECTOR, "a.app-jenkins-logo").click()
-
-    all_items = browser.find_elements(By.CSS_SELECTOR, "a.jenkins-table__link")
-    all_itemnames = [item.text for item in all_items]
-
-    assert ITEM_NAME in all_itemnames
-
-
-@pytest.mark.dependency(depends=["test_create_new_pipeline"])
-def test_message_field_cannot_be_empty(browser, wait):
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[.//span[normalize-space()='New Item']]"))
-    ).click()
-    browser.find_element(By.CSS_SELECTOR, "input#name").send_keys("")
-    browser.find_element(By.XPATH, "//li[.//span[normalize-space()='Pipeline']]").click()
-    message = wait.until(
-        EC.visibility_of_element_located(
-            (By.XPATH, "//div[@id='itemname-required']"))
+def test_create_new_pipeline(browser):
+    project_page = (
+        HomePage(browser)
+        .new_item_click()
+        .set_project_name(PIPELINE_NAME)
+        .select_pipeline_and_ok_click()
+        .set_description(DESCRIPTION)
+        .save()
     )
 
-    assert message.text == "» This field cannot be empty, please enter a valid name"
+    assert project_page.get_description() == DESCRIPTION
+    assert project_page.get_project_name() == PIPELINE_NAME
 
-
-@pytest.mark.dependency(depends=["test_message_field_cannot_be_empty"])
-def test_message_not_allowed_name(browser, wait):
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[.//span[normalize-space()='New Item']]"))
-    ).click()
-    browser.find_element(By.CSS_SELECTOR, "input#name").send_keys(".")
-    message = wait.until(
-        EC.visibility_of_element_located(
-            (By.XPATH, "//div[@id='itemname-invalid']"))
-    )
-
-    assert message.text == "» “.” is not an allowed name"
-
-
-@pytest.mark.dependency(depends=["test_message_not_allowed_name"])
-def test_message_unsafe_character(browser, wait):
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[.//span[normalize-space()='New Item']]"))
-    ).click()
-    for _ in ["?", "*", "/", "!", "%", "$", "&", ";", ":"]:
-        browser.find_element(By.CSS_SELECTOR, "input#name").clear()
-        browser.find_element(By.CSS_SELECTOR, "input#name").send_keys(f"{_}")
-        message = wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, f"//div[@id='itemname-invalid']"))
-        )
-
-        assert message.text == f"» ‘{_}’ is an unsafe character"
-
-
-@pytest.mark.dependency(depends=["test_message_unsafe_character"])
-def test_message_job_already_exists(browser, wait):
-    wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[.//span[normalize-space()='New Item']]"))
-    ).click()
-    browser.find_element(By.CSS_SELECTOR, "input#name").send_keys(ITEM_NAME)
-    message = wait.until(
-        EC.visibility_of_element_located(
-            (By.XPATH, "//div[@id='itemname-invalid']"))
-    )
-
-    assert message.text == "» A job already exists with the name ‘Test Pipeline’"
