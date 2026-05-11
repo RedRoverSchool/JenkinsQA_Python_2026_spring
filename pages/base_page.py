436bdd67075a0fbbcbc53205028f11a3f4996185
kiramitt <kiramitt@gmail.com>
2026-05-11 18:22:51 +0300

    RF_01.002.04 | New Item > Folder > test_create_folder to POM

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,3 +5,6 @@ class BasePage:
     def __init__(self, driver, timeout=10):
         self.driver = driver
         self.wait10 = WebDriverWait(driver, timeout)
+
+    def get_current_url(self):
+        return self.driver.current_url
