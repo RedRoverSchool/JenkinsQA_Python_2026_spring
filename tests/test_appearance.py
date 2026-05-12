50e0ff9b1dfc592283f932306ad0259452f47906
Dyb87 <dybin87@gmail.com>
2026-05-12 23:02:48 +0300

    add dependency to test appearance

--- tests/test_appearance.py
+++ tests/test_appearance.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_appearance_to_dark(browser):
     themes={'none': 'dark', 'dark': 'dark-system', 'dark-system': 'none'}
@@ -45,7 +47,8 @@ def test_appearance_to_light(browser):
     assert theme != theme_new
 
 
-@pytest.mark.skip()
+
+@pytest.mark.dependency(depends=["test_create_pipeline_project"])
 def test_appearance_show_pipeline_stages(browser):
     browser.find_element(By.XPATH, "//a[@id='root-action-ManageJenkinsAction']").click()
     browser.find_element(By.XPATH, "//a[@href='appearance']").click()
@@ -56,10 +59,8 @@ def test_appearance_show_pipeline_stages(browser):
     checkbox_stages_1.click()
 
     browser.get("http://localhost:8080/")
-    job_link = browser.find_element(By.XPATH, '//a[@href="job/test/"]')
+    job_link = browser.find_element(By.XPATH, '//a[@href="job/test_1/"]')
     browser.execute_script("arguments[0].click();", job_link)
     stages_loc = browser.find_element(By.XPATH, "//a[@href='multi-pipeline-graph']")
     assert stages_loc.is_displayed()
-
-
 
