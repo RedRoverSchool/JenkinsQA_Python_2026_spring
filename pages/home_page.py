d3ff4ce10e058c4ecfa56ec0a549965cdb8273ce
kiramitt <kiramitt@gmail.com>
2026-05-12 11:57:57 +0300

    edit test

--- pages/home_page.py
+++ pages/home_page.py
@@ -15,5 +15,5 @@ class HomePage(BasePage):
 
     def get_projects_names_list(self):
         project_elements = self.driver.find_elements(By.CLASS_NAME, "jenkins-table__link")
-        projects_names = [element.text for element in project_elements]
-        return projects_names
+        project_names = [element.text for element in project_elements]
+        return project_names
--- tests/test_folder.py
+++ tests/test_folder.py
@@ -25,18 +25,18 @@ def create_folder(driver, name, full_creation=True):
 
 @pytest.mark.dependency()
 def test_create_folder(browser):
-    projects_names_list = (
+    project_names_list = (
         HomePage(browser)
         .new_item_click()
         .set_project_name(FOLDER_NAME)
         .select_folder_and_ok_click()
         .save_click()
         .go_home_page()
-        .get_projects_names_list()
+        .get_project_names_list()
     )
 
-    assert len(projects_names_list) > 0
-    assert projects_names_list[0] == FOLDER_NAME
+    assert len(project_names_list) > 0
+    assert project_names_list[0] == FOLDER_NAME
 
 
 def test_create_folder_with_display_name(browser):
