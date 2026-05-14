64122d2f23d6a0c7f836af0f67e17fa4edd0dafb
Jana Demashkevitch <jana.demaskevits@outlook.com>
2026-05-14 08:01:16 +0300

    fix: update PR changes

--- tests/test_freestyle_project2.py
+++ tests/test_freestyle_project2.py
@@ -4,41 +4,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+FREESTYLE_PROJECT_NAME = "Test_Freestyle"
 
 @pytest.mark.dependency
 def test_create_freestyle_project(browser):
     wait = WebDriverWait(browser, 10)
 
     browser.find_element(By.XPATH, "//a[@href='/view/all/newJob']").click()
-    browser.find_element(By.ID, "name").send_keys("Test Freestyle")
+    browser.find_element(By.ID, "name").send_keys(FREESTYLE_PROJECT_NAME)
     browser.find_element(By.CLASS_NAME, "hudson_model_FreeStyleProject").click()
     browser.find_element(By.ID, "ok-button").click()
 
-    submit_btn = wait.until(
+    wait.until(
         EC.element_to_be_clickable((By.NAME, "Submit"))
-    )
-    submit_btn.click()
+    ).click()
 
-    assert browser.find_element(By.CSS_SELECTOR, ".job-index-headline.page-headline").text == "Test Freestyle"
+    assert browser.find_element(By.CSS_SELECTOR, ".job-index-headline.page-headline").text == FREESTYLE_PROJECT_NAME
 
 
 @pytest.mark.dependency(depends=["test_create_freestyle_project"])
 def test_description_preview(browser):
     wait = WebDriverWait(browser, 10)
 
-    freestyle_project = browser.find_element(By.XPATH, "//a[@href='job/Test%20Freestyle/']")
+    freestyle_project = browser.find_element(By.CSS_SELECTOR, f'[href="job/{FREESTYLE_PROJECT_NAME}/"]')
     ActionChains(browser).move_to_element(freestyle_project).perform()
-    browser.find_element(By.CSS_SELECTOR, '[id="job_Test Freestyle"] .jenkins-menu-dropdown-chevron').click()
+    browser.find_element(By.CSS_SELECTOR, '[id="job_Test_Freestyle"] .jenkins-menu-dropdown-chevron').click()
 
-    config_link = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//a[@href='/job/Test%20Freestyle/configure']"))
-    )
-    config_link.click()
+    wait.until(
+        EC.element_to_be_clickable((By.XPATH, f"//a[@href='/job/{FREESTYLE_PROJECT_NAME}/configure']")
+    )).click()
 
     description_textarea = browser.find_element(By.XPATH, "//textarea[@name='description']")
     description_textarea.send_keys("test preview")
-    preview_btn = browser.find_element(By.CLASS_NAME, "textarea-show-preview")
-    preview_btn.click()
+    browser.find_element(By.CLASS_NAME, "textarea-show-preview").click()
 
     preview_textarea = wait.until(
         EC.visibility_of_element_located((By.CLASS_NAME, "textarea-preview"))
