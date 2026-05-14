84b5137e0fe3abc3255ddd1718e7bd2604dff11f
ElenaDetusheva <e.det@list.ru>
2026-05-14 20:45:15 +0300

    add rf test_add_description_after_creation

--- pages/folder_page.py
+++ pages/folder_page.py
@@ -5,6 +5,7 @@ from pages.project_page import ProjectPage
 
 
 class FolderPage(ProjectPage):
+
     def get_config_description(self):
         return self.wait10.until(EC.visibility_of_element_located((By.ID, "view-message"))).text
 
@@ -21,3 +22,19 @@ class FolderPage(ProjectPage):
         self.wait10.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".job-index-headline.page-headline")))
         return [line for line in self.driver.find_element(By.ID, "main-panel").text.split('\n') if
                 line.startswith("Full folder name: ")][0]
+
+    def click_add_description_link(self):
+        self.wait10.until(EC.visibility_of_element_located((By.ID, "description-link"))).click()
+
+        return self
+
+    def add_description(self, new_description):
+        self.wait10.until(EC.visibility_of_element_located((By.NAME, "description"))).send_keys(new_description)
+        self.wait10.until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
+
+        return self
+
+    def get_description_content(self):
+        self.wait10.until(EC.visibility_of_element_located((By.ID, "description-content")))
+
+        return self.driver.find_element(By.ID, "description-content").text
