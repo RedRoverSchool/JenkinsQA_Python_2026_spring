3c3d943464cdf06b7890023f994f0130eb4cc8f9
Евгения (kiramitt) <73468778+kiramitt@users.noreply.github.com>
2026-05-14 16:08:03 +0300

    RF_01.002.07 | New Item > Folder > test_create_nested_folder to POM (#772)

--- pages/folder_page.py
+++ pages/folder_page.py
@@ -7,3 +7,17 @@ from pages.project_page import ProjectPage
 class FolderPage(ProjectPage):
     def get_config_description(self):
         return self.wait10.until(EC.visibility_of_element_located((By.ID, "view-message"))).text
+
+    def new_item_click(self):
+        from pages.new_item_page import NewItemPage
+
+        self.wait10.until(
+            EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, '/newJob')]"))
+        ).click()
+
+        return NewItemPage(self.driver)
+
+    def get_full_folder_name(self):
+        self.wait10.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".job-index-headline.page-headline")))
+        return [line for line in self.driver.find_element(By.ID, "main-panel").text.split('\n') if
+                line.startswith("Full folder name: ")][0]
