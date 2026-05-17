0f7a16c49a4ec54fcd255d9cb4779de684a3049b
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-05-17 12:21:13 +0300

    RF_04.001.01_02 || Verify that toggle has tooltip | Verify availability the message on the Status Pag move to  POM
    UPD resolve conflict. Add changes from main
    
    Move to POM
    1. test_verify_enable_toggle_has_tooltip
    2. test_verify_status_switching_enable_toggle
    
     1. Create def method click_enable_toggle and get_text_toggle_tooltip  in MulticonfigurationProjectConfigPage
     2. Create def method get_disable_project_message in MulticonfigurationProjectPage

---
+++ pages/base_config_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from pages.base_page import BasePage
+from pages.base_project_page import BaseProjectPage
+from pages.folder_page import FolderPage
+from pages.freestyle_project_page import FreestyleProjectPage
+
+class BaseConfigPage(BasePage):
+    def click_save(self, job_type="project"):
+        button = self.driver.find_element(By.NAME, "Submit")
+        button.click()
+        self.wait10.until(EC.staleness_of(button))
+        if job_type == "project":
+            return BaseProjectPage(self.driver)
+        elif job_type == "folder":
+            return FolderPage(self.driver)
+        elif job_type == "freestyle_project":
+            return FreestyleProjectPage(self.driver)
+        return None
+
+    def set_description(self, description):
+        self.driver.find_element(By.XPATH, "//textarea[contains(@name, 'description')]").send_keys(description)
+
+        return self
