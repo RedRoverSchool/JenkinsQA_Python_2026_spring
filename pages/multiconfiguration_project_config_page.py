2e6849fac51f555345ef56e45d9b9d9000fd026a
Vyacheslav Belyaev <vbelyaev86@gmail.com>
2026-05-17 13:31:56 +0300

    rf_04.001.01_02 || Verify that toggle has tooltip | Verify availability the message on the Status Pag move to  POM
    New PR
    UPD resolve conflict. Add changes from main
    
    Move to POM
    1. test_verify_enable_toggle_has_tooltip
    2. test_verify_status_switching_enable_toggle
    
     1. Create def method click_enable_toggle and get_text_toggle_tooltip  in MulticonfigurationProjectConfigPage
     2. Create def method get_disable_project_message in MulticonfigurationProjectPage

--- pages/multiconfiguration_project_config_page.py
+++ pages/multiconfiguration_project_config_page.py
@@ -1,5 +1,25 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 from pages.base_config_page import BaseConfigPage
+from selenium.webdriver.support import expected_conditions as EC
+from pages.multiconfiguration_project_page import MulticonfigurationProjectPage
 
 
 class MulticonfigurationProjectConfigPage(BaseConfigPage):
-    pass
+    def click_save_button(self):
+        self.wait10.until(EC.element_to_be_clickable((By.NAME, "Submit"))).click()
+
+        return MulticonfigurationProjectPage(self.driver)
+
+    def click_enable_toggle(self):
+        self.wait10.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "#toggle-switch-enable-disable-project > label"))).click()
+
+        return self
+
+    def get_text_toggle_tooltip(self):
+        enabled_toggle = self.driver.find_element(By.ID, "toggle-switch-enable-disable-project")
+
+        ActionChains(self.driver).move_to_element(enabled_toggle).perform()
+
+        return self.wait10.until(EC.visibility_of_element_located((By.CLASS_NAME, "tippy-content"))).text
