5d6f6fbb28130c8dbd9dd3eeb9dc7366c11f2051
Alona Kot <90215479+AlonaKot@users.noreply.github.com>
2026-05-18 16:01:45 +0200

    feat: files from the main was returned back

--- pages/manage_page.py
+++ pages/manage_page.py
@@ -3,14 +3,21 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from pages.base_page import BasePage
 from pages.credentials_page import CredentialsPage
+from pages.users_page import UsersPage
 from pages.tools_page import ToolsPage
 
 
 class ManagePage(BasePage):
+
     def credentials_click(self):
         self.wait10.until(EC.element_to_be_clickable((By.XPATH, "//*[@href ='credentials']"))).click()
 
         return CredentialsPage(self.driver)
+
+    def click_users(self) -> UsersPage:
+        self.wait10.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='securityRealm/']"))).click()
+
+        return UsersPage(self.driver)
 
     def tools_click(self):
         self.wait10.until(
--- pages/multiconfiguration_project_page.py
+++ pages/multiconfiguration_project_page.py
@@ -1,5 +1,12 @@
+
 from pages.base_project_page import BaseProjectPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class MulticonfigurationProjectPage(BaseProjectPage):
-    pass
+    def get_disable_project_message(self):
+        return self.wait10.until(EC.visibility_of_element_located((By.ID, "enable-project"))).text
+
+    def get_project_name(self):
+        return self.wait10.until(EC.visibility_of_element_located((By.TAG_NAME, "h1"))).text
