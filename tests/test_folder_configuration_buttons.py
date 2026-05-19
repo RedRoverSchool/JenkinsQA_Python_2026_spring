9d969d52df7b6b1c27d2ebae55796c8bbc17494f
Mik <a1127609817@gmail.com>
2026-05-19 01:51:27 -0300

    Add test_new_items_cycle

--- tests/test_folder_configuration_buttons.py
+++ tests/test_folder_configuration_buttons.py
@@ -52,5 +52,6 @@ def test_config_apply(browser):
     wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='notification-bar']//span[text()='Saved']")))
     wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@name="Apply"]'))).click()
     wait.until(EC.presence_of_element_located((By.XPATH , "//*[@id='notification-bar']//span[text()='Saved']")))
+    WebDriverWait(browser, 10).until(EC.url_changes(browser.current_url))
 
     assert "configure" in browser.current_url, "Apply применяется, редиректа нет"
