6af1dcf4e6de584ac844e6b3a9ac6ac5016a6d89
Mik <a1127609817@gmail.com>
2026-05-29 17:52:42 -0300

    Add test_folder_buttons

--- tests/test_folder_configuration.py
+++ tests/test_folder_configuration.py
@@ -22,3 +22,29 @@ def test_add_display_name_to_folder(browser):
 
     assert len(project_names_list) > 0
     assert project_names_list[0] == DISPLAY_NAME
+
+
+@pytest.mark.dependency(depends=['test_create_folder'])
+def test_folder_apply_button(browser):
+    apply_result = (
+        HomePage(browser)
+        .click_project_name(FOLDER_NAME, 'folder')
+        .click_project_configure('folder')
+        .set_display_name("Display_name")
+        .click_apply()
+        .get_notification_saved()
+    )
+
+    assert apply_result == "Saved"
+    assert "configure" in browser.current_url, "User was redirected from the configuration page after clicking Apply"
+
+@pytest.mark.dependency(depends=['test_folder_apply_button'])
+def test_folder_save_button(browser):
+    save_result = (HomePage(browser)
+        .click_project_name(FOLDER_NAME, 'folder')
+        .click_project_configure('folder')
+        .set_display_name("Display_name")
+        .click_save('folder')
+    )
+
+    assert "configure" not in browser.current_url, "Error: Save button failed to redirect the user to the Folder's main page!"
