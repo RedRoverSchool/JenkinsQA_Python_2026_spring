2d249d4c9c0fa0ede04cda3d65e64ad7da707a1a
ElenaDetusheva <e.det@list.ru>
2026-06-09 12:15:21 +0300

    add time.sleep

--- tests/test_view.py
+++ tests/test_view.py
@@ -3,7 +3,6 @@ import pytest
 from pages.home_page import HomePage
 
 def test_create_new_view(browser):
-
     new_view_name = "New_view"
 
     view_names_list = (
@@ -20,5 +19,6 @@ def test_create_new_view(browser):
         .go_home_page()
         .get_view_tab_names()
     )
+
     assert new_view_name in view_names_list
 
