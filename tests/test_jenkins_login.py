0dfe739650916662fd87b7172a866cb78db85e34
RAYAN_MUHAMETZYANOV <rim2005mir@gmail.com>
2026-06-27 01:33:56 +0300

    Refactor Jenkins login page UI test

--- tests/test_jenkins_login.py
+++ tests/test_jenkins_login.py
@@ -1,17 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+def test_login_page_title(browser):
 
 
-def test_jenkins_login_page_opens():
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
-    )
 
-    driver.get("http://localhost:8080/login")
 
-    assert driver.title is not None
-    assert "Jenkins" in driver.title
-    assert "login" in driver.current_url
-
-    driver.quit()
+    assert browser.title is not None
+    assert "Jenkins" in browser.title
+    assert "login" in browser.current_url
